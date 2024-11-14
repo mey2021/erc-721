18f642abfd5f014477691e1a59a72ba80f16300f
mey2021 <mey2021@live.com>
2024-11-15 00:13:23 +0330

    _

---
+++ contracts/MyToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MyToken is ERC721 {
+    constructor() ERC721("MyToken", "MTK") {}
+
+    uint256 public tokenId;
+    uint256 public maxSupply = 1000;
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "My NFT", "description": "Whatever", "image": "https://ethereumhacker.com/img/nft.png"}'
+                    )
+                )
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    function mint() public {
+        require(tokenId < maxSupply, "All tokens have been minted");
+        _safeMint(msg.sender, tokenId);
+        tokenId = tokenId + 1;
+    }
+}
