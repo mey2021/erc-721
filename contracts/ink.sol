6bd378c6c277ecf0bb0942b83f9662601316604d
mey2021 <mey2021@live.com>
2024-12-01 21:27:06 +0330

    _

--- contracts/ink.sol
+++ contracts/ink.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
  
 contract InkContract {
-    string public greeting = "Hello, Ink!";
+    string public greeting = "Hello, Ink again!";
     
     function setGreeting(string memory _greeting) public {
         greeting = _greeting;
